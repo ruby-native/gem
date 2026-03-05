304c3835753279f88472c9ae7150c4c5f8f098e7
Joe Masilotti <joe@masilotti.com>
2026-03-04 18:12:01 -0800

    Potential Cloudflare workaround

--- app/controllers/ruby_native/auth/sessions_controller.rb
+++ app/controllers/ruby_native/auth/sessions_controller.rb
@@ -20,8 +20,27 @@ module RubyNative
           response.headers["set-cookie"] = cookies.join("\n")
         end
 
-        Rails.logger.info { "[RubyNative] OAuth token exchanged with #{cookies.size} cookies, redirecting to #{data[:redirect_url]}" }
-        redirect_to data[:redirect_url], allow_other_host: true
+        redirect_url = data[:redirect_url]
+        Rails.logger.info { "[RubyNative] OAuth token exchanged with #{cookies.size} cookies, redirecting to #{redirect_url}" }
+
+        # Render a 200 HTML page instead of a 302 redirect. CDNs like
+        # Cloudflare can strip Set-Cookie headers from redirect responses,
+        # preventing WKWebView from applying them. A 200 response guarantees
+        # cookies are set before the meta refresh navigates to the target.
+        render html: redirect_page(redirect_url).html_safe, status: :ok
+      end
+
+      private
+
+      def redirect_page(url)
+        escaped = ERB::Util.html_escape(url)
+        <<~HTML
+          <!DOCTYPE html>
+          <html>
+          <head><meta http-equiv="refresh" content="0;url=#{escaped}"></head>
+          <body></body>
+          </html>
+        HTML
       end
     end
   end
